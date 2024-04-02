c188ff91b4cb3de9795952cf1b99e23d533c251f
chris <jimenezchristopher5@gmail.com>
2024-04-02 14:20:46 -0500

    created computer action phase and made it into a function

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const prompt = require('prompt-sync')();
 //Game Introduction
-console.log(`************
+console.log(`********************
 Welcome to Castle Battle! 
-**********`); //Add stars later
+********************`); //Add stars later
 console.log(``); //We want space between text - for aesthetic visual purposes
 //We want the player to have the option of reading the lore and whether they want to read the instructions or not
 let intro = prompt(`Would you like to read the lore? - Type (Y) for yes (N) for no: `);
@@ -39,7 +39,7 @@ let playerHealth = 10;
 let computerHealth = 10;
 let computerNumber = Math.floor(Math.random() * 5) + 1; //We chose to mulitply by 5 and then add 1 to ensure that we NEVER get 0 (since coputer needs to choose 1-5) and 5 is inclusive 
 const computerOptions = ["attack", "repair"];
-//let computerAction = computerOptions[Math.floor(Math.random() * 2)]; //We will use later to gnerate the action computer will take
+// let computerAction = computerOptions[Math.floor(Math.random() * 2)]; //We will use later to gnerate the action computer will take
 let initialPeon = prompt(`Attention! Before the game starts with round 1, you need to create your first peon - Give your peon a name: `);
 playerBarracks.push({name: initialPeon, job: "nothing"});
 console.log(`Congrats! You've created your first peon and this is the current status of your barracks:`, playerBarracks);
@@ -114,6 +114,7 @@ if(round === "create"){
 console.log(playerBarracks);
 }
 startRound();
+console.clear();
 
 //Part 2: Player action
 playerBarracks.forEach((peon)=>{ //We want to iterate over the player's barracks to initiate the action phase 
@@ -126,10 +127,21 @@ playerBarracks.forEach((peon)=>{ //We want to iterate over the player's barracks
     }
 })
 
+//Part 3: 
+const computerAction = ()=>{
+    let computerDecision = computerOptions[Math.floor(Math.random() * 2)];
+    if(computerDecision === computerOptions[0]){
+        playerHealth -= computerNumber;
+        console.log(`Oh no! The Feudal Lord has inflicted damage and reduced your health by ${computerNumber} points`);
+    }else if(computerDecision === computerOptions[1]){
+        computerHealth += computerNumber;
+        console.log(`Watch out! The Feudal Lord has chosen to heal and increased his health by ${computerNumber} points`);
+    }
+}
+computerAction();
+console.log(`Current Player Health: ${playerHealth} - Current Feudal Lord Health: ${computerHealth}`);
 
 
-
-// console.log(playerHealth, computerHealth);
 
 
 
